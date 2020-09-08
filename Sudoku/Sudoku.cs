9c5a9bad4399b334aaa51290f97a471d1c058679
Manfredo Manfredi <manfredo.manfredi@gmail.com>
2020-09-08 17:49:12 +0200

    CheckBox

--- Sudoku/Sudoku.cs
+++ Sudoku/Sudoku.cs
@@ -67,5 +67,62 @@ namespace Sudoku
             }
             return true;
         }
+
+        public bool CheckVertical(int n, int i, int j)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                if (x == i) continue;
+                if (n == IntGrid[j, x])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool CheckBox(int n, int i, int j)
+        {
+            int xBox = -1;
+            if(i >= 0 && i < 3)
+            {
+                xBox = 0;
+            } else if (i >= 3 && i < 6)
+            {
+                xBox = 1;
+            } else if(i >= 6 && i < 9)
+            {
+                xBox = 2;
+            }
+
+            int yBox = -1;
+            if(j >= 0 && j < 3)
+            {
+                yBox = 0;
+            } else if(j >= 3 && j < 6)
+            {
+                yBox = 1;
+            } else if (j >= 6 && j < 9)
+            {
+                yBox = 2;
+            }
+
+            for(int x = xBox*3; x < xBox*3 + 3; x++)
+            {
+                for(int y = yBox*3; y < yBox*3+3; y++)
+                {
+                    if(x == i && y == j)
+                    {
+                        continue;
+                    }
+                    if(IntGrid[x,y] == n)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
     }
 }
