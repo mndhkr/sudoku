0f51100071c5d2322cf6ed4d6f3d6bc11f5efdd8
Manfredo Manfredi <manfredo.manfredi@gmail.com>
2020-09-09 09:28:27 +0200

    Messed-up but functioning

--- Sudoku/Sudoku.cs
+++ Sudoku/Sudoku.cs
@@ -14,6 +14,15 @@ namespace Sudoku
         private Int32[,] IntGrid { get; set; }
         private LinkedList<int>[,] CandidatesGrid { get; set; }
 
+        private Int32[,] SavedState { get; set; }
+        private int LastTriedCandidate { get; set; }
+        private bool StateSaved { get; set; } = false;
+
+        private int Zeroes { get; set; } = 0;
+        private bool Modified { get; set; } = false;
+
+        private bool Guessed { get; set; } = false;
+
         public Sudoku()
         {
             Grid = new TextBox[9, 9];
@@ -68,9 +77,66 @@ namespace Sudoku
             {
                 for (int y = 0; y < 9; y++)
                 {
-                    Grid[x, y].Text = IntGrid[x, y].ToString();
+                    Grid[x, y].Text = (IntGrid[x, y] == 0) ? "X" : IntGrid[x, y].ToString();
                 }
             }
+        }
+
+        private void SaveState()
+        {
+            SavedState = new int[9, 9];
+
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    SavedState[x, y] = IntGrid[x, y];
+                }
+            }
+            StateSaved = true;
+        }
+
+        private void LoadState()
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    IntGrid[x, y] = SavedState[x, y];
+                }
+            }
+            StateSaved = false;
+        }
+
+        private bool CheckChanges()
+        {
+            int newZeroes = CountZeroes();
+            if (this.Zeroes != newZeroes)
+            {
+                this.Zeroes = newZeroes;
+                this.Modified = true;
+            }
+            else
+            {
+                this.Modified = false;
+            }
+            return this.Modified;
+        }
+
+        private int CountZeroes()
+        {
+            int count = 0;
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    if (IntGrid[x, y] == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
         }
 
         public void Solve()
@@ -79,27 +145,82 @@ namespace Sudoku
             for (int i = 0; i < 100; i++)
             {
                 PrintIntGrid();
+
                 FindCandidates();
                 FixCandidates();
+
                 PrintIntGrid();
                 Console.WriteLine();
+
                 InitCandidates();
                 FindCandidates();
-                FixRemainders();
+
+                FixRemaindersRow();
+
                 InitCandidates();
                 FindCandidates();
                 FixCandidates();
+
+                FixRemaindersColumn();
+
+                InitCandidates();
+                FindCandidates();
+                FixCandidates();
+
+                FixRemaindersBox();
+
+                InitCandidates();
+                FindCandidates();
+                FixCandidates();
+
                 PrintIntGrid();
                 Console.WriteLine();
-                FillMandatory();
+
+                FillMandatoryinColumn();
+
                 InitCandidates();
                 FindCandidates();
                 FixCandidates();
-                if (!hasCandidates())
-                    break;
+
+                FillMandatoryinLine();
+
                 InitCandidates();
+                FindCandidates();
+                FixCandidates();
+
+                FillMandatoryInBox();
+
+                InitCandidates();
+                FindCandidates();
+                FixCandidates();
+
+                //if (!hasCandidates())
+                //    break;
+                //InitCandidates();
+
+                if (!this.CheckChanges() && this.Zeroes == 0)
+                {
+                    Console.WriteLine("Finito!");
+                }
+                //else if(!this.CheckChanges() && !this.Guessed)
+                //{
+                //    Console.WriteLine("Non so più cosa fare... ({0})", i);
+                //    Console.WriteLine("Procedo per tentativi...");
+
+                //    Guess();
+                //} else if (!this.CheckChanges() && !this.Guessed)
+                //{
+                //    if (this.StateSaved)
+                //    {
+                //        LoadState();
+                //        this.StateSaved = false;
+                //    }
+                //    Guess();
+                //}
+                InitCandidates();
+                CopyIntToTextBoxes();
             }
-            CopyIntToTextBoxes();
+
         }
 
         public bool hasCandidates()
@@ -128,6 +249,26 @@ namespace Sudoku
                 }
             }
             return true;
+        }
+
+        private void Guess()
+        {
+            if (!this.Guessed)
+                this.SaveState();
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    if (CandidatesGrid[x, y].Count > 1)
+                    {
+                        IntGrid[x, y] = CandidatesGrid[x, y].ElementAt((new Random()).Next(0, CandidatesGrid[x, y].Count - 1));
+                        CandidatesGrid[x, y].Clear();
+                        Console.WriteLine("Provo con {0}, in {1},{2}", IntGrid[x, y], x + 1, y + 1);
+                        return;
+                    }
+                }
+            }
+            this.Guessed = true;
         }
 
         public bool CheckVertical(int n, int i, int j)
@@ -216,7 +357,7 @@ namespace Sudoku
                         if (CheckOrizontal(n, x, y) && CheckVertical(n, x, y) && CheckBox(n, x, y))
                         {
                             CandidatesGrid[x, y].AddLast(n);
-                            Console.WriteLine("Candidato: {0}, in {1},{2}", n, x, y);
+                            Console.WriteLine("Candidato: {0}, in {1},{2}", n, x + 1, y + 1);
                         }
                     }
                 }
@@ -234,13 +375,13 @@ namespace Sudoku
                         int n = CandidatesGrid[x, y].First();
                         IntGrid[x, y] = n;
                         CandidatesGrid[x, y].RemoveFirst();
-                        Console.WriteLine("Fisso da Candidati: {0}, in {1},{2}", n, x, y);
+                        Console.WriteLine("Fisso da Candidati: {0}, in {1},{2}", n, x + 1, y + 1);
                     }
                 }
             }
         }
 
-        public void FixRemainders()
+        public void FixRemaindersRow()
         {
             bool stop = false;
 
@@ -277,10 +418,11 @@ namespace Sudoku
                                 {
                                     IntGrid[i, j] = n;
                                     CandidatesGrid[i, j].Clear();
-                                    Console.WriteLine("Fisso Mancante Riga: {0} in {1},{2}", n, i, j);
+                                    Console.WriteLine("Fisso Mancante Riga: {0} in {1},{2}", n, i + 1, j + 1);
                                     stop = true;
                                     break;
-                                } else
+                                }
+                                else
                                 {
                                     found = false;
                                 }
@@ -289,9 +431,12 @@ namespace Sudoku
                     }
                 }
             }
+        }
 
+        public void FixRemaindersColumn()
+        {
 
-            stop = false;
+            bool stop = false;
 
 
             //Controllo se nelle colonne verticali manca un solo numero
@@ -327,10 +472,11 @@ namespace Sudoku
                                 {
                                     IntGrid[i, j] = n;
                                     CandidatesGrid[i, j].Clear();
-                                    Console.WriteLine("Fisso Mancante Colonna: {0} in {1},{2}", n, i, j);
+                                    Console.WriteLine("Fisso Mancante Colonna: {0} in {1},{2}", n, i + 1, j + 1);
                                     stop = true;
                                     break;
-                                } else
+                                }
+                                else
                                 {
                                     found = false;
                                 }
@@ -339,8 +485,12 @@ namespace Sudoku
                     }
                 }
             }
+        }
 
-            stop = false;
+        public void FixRemaindersBox()
+        {
+
+            bool stop = false;
 
 
             // Controllo se nel box manca un solo numero....
@@ -376,7 +526,7 @@ namespace Sudoku
                                         {
                                             for (int j = yBox * 3; j < yBox * 3 + 3; j++)
                                             {
-                                               if(IntGrid[i,j] == n)
+                                                if (IntGrid[i, j] == n)
                                                 {
                                                     found = true;
                                                     break;
@@ -387,14 +537,15 @@ namespace Sudoku
                                                 break;
                                             }
                                         }
-                                        if(!found)
+                                        if (!found)
                                         {
                                             IntGrid[x, y] = n;
                                             CandidatesGrid[x, y].Clear();
-                                            Console.WriteLine("Fisso Box: {0} in {1},{2}", n, x, y);
+                                            Console.WriteLine("Fisso Box: {0} in {1},{2}", n, x + 1, y + 1);
                                             stop = true;
                                             break;
-                                        } else
+                                        }
+                                        else
                                         {
                                             found = false;
                                         }
@@ -407,34 +558,38 @@ namespace Sudoku
             }
         }
 
+
         private void FillMandatoryInBox()
         {
-            for (int n = 0; n <= 9; n++)
+            for (int n = 1; n <= 9; n++)
             {
                 for (int xBox = 0; xBox < 3; xBox++)
                 {
-                    int count = 0;
-                    int _x = 0, _y = 0;
                     for (int yBox = 0; yBox < 3; yBox++)
                     {
+                        int count = 0;
+                        int _x = 0, _y = 0;
+
                         for (int x = xBox * 3; x < xBox * 3 + 3; x++)
                         {
                             for (int y = yBox * 3; y < yBox * 3 + 3; y++)
                             {
-                                if (IntGrid[x,y] == 0 && CandidatesGrid[x, y].Contains(n))
+                                if (IntGrid[x, y] == 0 && CandidatesGrid[x, y].Contains(n))
                                 {
+                                    Console.WriteLine("CandidatesGrid[{0},{1}].Contains({2}).", x + 1, y + 1, n);
                                     count++;
                                     _x = x;
                                     _y = y;
                                 }
                             }
                         }
-                    }
-                    if (count == 1)
-                    {
-                        IntGrid[_x, _y] = n;
-                        CandidatesGrid[_x, _y].Clear();
-                        Console.WriteLine("Fisso Box unico valido: {0} in {1},{2}", n, _x, _y);
+
+                        if (count == 1)
+                        {
+                            IntGrid[_x, _y] = n;
+                            CandidatesGrid[_x, _y].Clear();
+                            Console.WriteLine("Fisso Box unico valido: {0} in {1},{2}", n, _x + 1, _y + 1);
+                        }
                     }
                 }
             }
@@ -442,16 +597,16 @@ namespace Sudoku
 
         private void FillMandatoryinLine()
         {
-            for(int n = 1; n <=9; n++)
+            for (int n = 1; n <= 9; n++)
             {
                 int count = 0;
                 int x = 0, y = 0;
 
                 for (int i = 0; i < 9; i++)
                 {
-                    for(int j = 0; j < 9; j++)
+                    for (int j = 0; j < 9; j++)
                     {
-                        if(IntGrid[i,j] == 0 && CandidatesGrid[i,j].Contains(n))
+                        if (IntGrid[i, j] == 0 && CandidatesGrid[i, j].Contains(n))
                         {
                             count++;
                             x = i;
@@ -464,7 +619,7 @@ namespace Sudoku
                 {
                     IntGrid[x, y] = n;
                     CandidatesGrid[x, y].Clear();
-                    Console.WriteLine("Fisso Riga unico valido: {0} in {1},{2}", n, x, y);
+                    Console.WriteLine("Fisso Riga unico valido: {0} in {1},{2}", n, x + 1, y + 1);
                 }
             }
         }
@@ -492,7 +647,7 @@ namespace Sudoku
                 {
                     IntGrid[x, y] = n;
                     CandidatesGrid[x, y].Clear();
-                    Console.WriteLine("Fisso Colonna unico valido: {0} in {1},{2}", n, x, y);
+                    Console.WriteLine("Fisso Colonna unico valido: {0} in {1},{2}", n, x + 1, y + 1);
                 }
             }
         }
